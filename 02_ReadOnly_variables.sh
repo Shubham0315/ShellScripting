60d012435803163158765c78716cbd77bdba6f3b
Shubham0315 <105341138+Shubham0315@users.noreply.github.com>
2024-04-12 01:06:07 +0530

    Create 02_ReadOnly_variables.sh

---
+++ 02_ReadOnly_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+readonly COLLEGE="Sinhgad College of Engineering"
+readonly HOST="Perf"
+
+echo "My college is $COLLEGE"
+echo "System host is $HOST"
+
+COLLEGE="COEP"
+HOST="Preprod"
