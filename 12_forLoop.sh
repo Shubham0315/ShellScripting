28040438f0c94e96602309f2ae3615a4297dc7c6
Shubham0315 <105341138+Shubham0315@users.noreply.github.com>
2024-04-25 18:25:49 +0530

    Create 12_forLoop.sh

---
+++ 12_forLoop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+        echo "Number is $i"
+done
+
+#################
+
+for name in Shubham Rohit Haudge
+do
+        echo "The names are $name"
+done
+
+################
+
+for j in {a..h}
+do
+        echo "Letters are $j"
+done
